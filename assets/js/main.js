25da21c5fca760b854776cb3037a47ac542617db
ilarioforcherio <ilarioforcherio@gmail.com>
2022-10-21 14:05:16 +0200

    add project + print number 1-100

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+/***Consegna:**
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+**Prima di partire a scrivere codice poniamoci qualche domanda:***/
+
+// stamp numeri da 1 a 100
+//multipli di 3 stampa fizz
+//multipli 5 stampa fizzbuzz
+
+
+//stampa 1-100
+for (let i = 0; i <= 100; i++ ){
+    console.log(i);
+}
